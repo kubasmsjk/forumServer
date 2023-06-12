b27ebef21409e1d6a13b30382084e2c6f4c00ed3
JakubMieczkowski(109 <jakm5000@wp.pl>
2023-06-12 17:37:57 +0200

    create Post entity

--- src/main/java/com/wi/pb/forum/post/Post.java
+++ src/main/java/com/wi/pb/forum/post/Post.java
@@ -1,12 +1,29 @@
 package com.wi.pb.forum.post;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.wi.pb.forum.infrastructure.Identifiable;
+import com.wi.pb.forum.user.dto.ForumUserDTO;
+import jakarta.persistence.*;
 
 @Entity
-@Table
-public class Post {
+@Table(name="Post")
+public class Post implements Identifiable<Long> {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String description;
+
+//    @OneToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "form_user_id", referencedColumnName = "id")
+//    private ForumUserDTO ForumUser;
+
+    @Override
+    public Long getId() {
+        return null;
+    }
+
+    @Override
+    public void setId(Long aLong) {
+
+    }
 }
