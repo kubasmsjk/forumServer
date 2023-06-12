1929489639db07c81191795536ff09ec32640aea
JakubMieczkowski(109 <jakm5000@wp.pl>
2023-06-12 17:37:09 +0200

    create category entity

--- src/main/java/com/wi/pb/forum/category/Category.java
+++ src/main/java/com/wi/pb/forum/category/Category.java
@@ -1,4 +1,33 @@
 package com.wi.pb.forum.category;
 
-public class Category {
+
+import com.wi.pb.forum.infrastructure.Identifiable;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Category")
+public class Category implements Identifiable<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void String(String name) {
+        this.name = name;
+    }
 }
