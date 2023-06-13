afd0828071749b66393a9445639967d94ad30315
JakubMieczkowski(109 <jakm5000@wp.pl>
2023-06-13 11:11:24 +0200

    remove useless imports

--- src/main/java/com/wi/pb/forum/post/controllers/CommentController.java
+++ src/main/java/com/wi/pb/forum/post/controllers/CommentController.java
@@ -1,14 +1,11 @@
 package com.wi.pb.forum.post.controllers;
 
 import com.wi.pb.forum.infrastructure.SimpleController;
-import com.wi.pb.forum.infrastructure.SimpleFacade;
 import com.wi.pb.forum.post.CommentFacade;
 import com.wi.pb.forum.post.dto.CommentDTO;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("api/comment")
