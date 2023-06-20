edb7f2b13aaaabf0422ded26b6c2b0569b06263a
JakubMieczkowski(109 <jakm5000@wp.pl>
2023-06-20 17:53:42 +0200

    Violation Notification functionality classes

--- src/main/java/com/wi/pb/forum/violationNotification/Post.java
+++ src/main/java/com/wi/pb/forum/violationNotification/Post.java
@@ -1,8 +1,6 @@
 package com.wi.pb.forum.violationNotification;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import org.hibernate.annotations.Immutable;
 
 @Immutable
@@ -13,4 +11,33 @@ class Post {
     @Id
     private Long id;
     private String description;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "forum_user_id", referencedColumnName = "id")
+    private ForumUser author;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ForumUser getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(ForumUser author) {
+        this.author = author;
+    }
 }
