0be427035d2031b2bb0956128313d6332a61ac6e
JakubMieczkowski(109 <jakm5000@wp.pl>
2023-06-20 17:50:38 +0200

    delete annotation

--- src/main/java/com/wi/pb/forum/user/ForumUserRepository.java
+++ src/main/java/com/wi/pb/forum/user/ForumUserRepository.java
@@ -1,11 +1,9 @@
 package com.wi.pb.forum.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface ForumUserRepository extends JpaRepository<ForumUser, Long> {
     Optional<ForumUser> findByUsername(String username);
 }
