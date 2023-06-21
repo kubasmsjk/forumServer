87dce43ae27f78c0426bc67fe305573a0a7a30a0
JakubMieczkowski(109 <jakm5000@wp.pl>
2023-06-21 21:05:25 +0200

    violation notification functionality classes

--- src/main/java/com/wi/pb/forum/violationNotification/MainPost.java
+++ src/main/java/com/wi/pb/forum/violationNotification/MainPost.java
@@ -1,6 +1,5 @@
 package com.wi.pb.forum.violationNotification;
 
-import com.wi.pb.forum.infrastructure.Identifiable;
 import jakarta.persistence.*;
 import org.hibernate.annotations.Immutable;
 
@@ -9,47 +8,22 @@ import java.util.Set;
 @Immutable
 @Table(name="MainPost")
 @Entity(name = "ViolationNotificationMainPost")
-class MainPost extends Post {
+class MainPost   {
 
+    @Id
+    private Long id;
+    private String description;
     private String title;
 
-    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
-    @JoinColumn(name = "category_id")
-    private Category category;
-
-    @ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "forum_user_main_post",
-            joinColumns = @JoinColumn(name = "main_post_id"),
-            inverseJoinColumns = @JoinColumn(name = "forum_user_id"))
-    private Set<ForumUser> viewers;
-
-
     public Long getId() {
-        return super.getId();
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public Set<ForumUser> getViewers() {
-        return viewers;
-    }
-
-    public void setViewers(Set<ForumUser> viewers) {
-        this.viewers = viewers;
     }
 }
